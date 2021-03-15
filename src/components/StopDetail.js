ef553e19be9cbd7376ab4941d2e736f42470aaba
Jonathan <jschack94@gmail.com>
2021-03-15 18:58:22 -0500

    final

--- src/components/StopDetail.js
+++ src/components/StopDetail.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import getFacilities from "../services/getFacilities";
 
+const ShowDetails = ({
+  match: {
+    params: { stopId },
+  },
+}) => {
+  const [facilities, setFacilities] = useState([]);
+  const [facility, setFacility] = useState([]);
 
-  
-const ShowDetails = () => {
-    return (
-        <div>
-        Facilities
-        </div>
-      );
-    };
-    
+  useEffect(() => {
+    getFacilities().then((newFacilities) => {
+      setFacilities(newFacilities);
+      const filtered = newFacilities.filter(
+        (facility) => facility?.relationships?.stop?.data?.id === stopId
+      ); //stopId= "place-alfcl"
+      console.log("filtered", filtered);
+      setFacility(filtered);
+    });
+  }, [stopId]);
+
+  return (
+    <div>
+      <strong>Facilities for {stopId}</strong>
+      <p></p>
+      {console.log(facilities)}
+      {console.log("facility is", facility)}
+      {facilities.map((fac) => (
+        <div>**{fac.attributes.short_name}**</div>
+      ))}
+    </div>
+  );
+};
 
 export default ShowDetails;
--- src/services/getFacilities.js
+++ src/services/getFacilities.js
@@ -1,10 +1,9 @@
 const getFacilities = id => {
-    const url = `https://api-v3.mbta.com/facilities?filter[place]=${id}`;
-  
-    return fetch(url)
-      .then(response => response.json())
-      .then(json => json.data);
-  };
-  
-  export default getFacilities;
-  
+  const url = `https://api-v3.mbta.com/facilities`;
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(json => json.data);
+};
+
+export default getFacilities;
